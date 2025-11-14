706fb15367cee5d44526cac9b9211c4de0db27a5
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-11-14 10:12:21 +0100

    creating homepage content

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,47 @@
+<footer>
+    <h2 class="sro" aria-level="2">
+        Footer
+    </h2>
+    <div>
+        <div class="informations-footer">
+            <h3>
+                LES PATTES HEUREUSES
+            </h3>
+            <p>
+                Responsable : Elise Lambot
+            </p>
+            <p>
+                Téléphone : 080 546 384
+            </p>
+            <p>
+                Adresse : Rue des bois 4, 4700 Malmédy
+            </p>
+            <p>
+                Ouvert : Lu à Ve — 10 à 17h
+            </p>
+        </div>
+        <nav>
+            <h3>
+                Navigation <div>de bas de page</div>
+            </h3>
+           <a href="{{--{{route('jiris.index')}}--}}"> ACCUEIL </a>
+            <a href="{{--{{route('projects.index')}}--}}"> NOS ANIMAUX </a>
+            <a href="{{--{{route('contacts.index')}}--}}"> CONTACT</a>
+
+        </nav>
+        <div>
+            <a href="">
+                Contactez-nous
+            </a>
+        </div>
+    </div>
+    <div>
+        <a href="">
+            Mentions Légales
+        </a>
+        <p>
+            Crée par Kreins Lisa-Marie
+        </p>
+    </div>
+
+</footer>
---
+++ resources/views/public/homepage.blade.php
@@ -0,0 +1,63 @@
+@component('layouts.app')
+
+    <section>
+        <h2>
+            LES PATTES HEUREUSES
+        </h2>
+        <p>
+            Ici nous soignons et donnent de l’espoir aux animaux abandonnés ou maltraités de la région de Malmedy et ses environs.
+        </p>
+        <img src="../../img/dogs.jpg" alt="Image avec deux chiens" width="948" height="911">
+    </section>
+    <section>
+        <div>
+            <h2>
+                Là où chaque patte compte
+            </h2>
+            <p>
+                Depuis 15 ans, le refuge "Les Pattes Heureuses" accueille et soigne les animaux abandonnés ou maltraités
+                de Malmedy et ses environs. Fondé par Élise Lambot, le refuge offre un havre de paix où compassion et
+                engagement se rencontrent. Chiens, chats et petits rongeurs y trouvent soins, chaleur et amour, en
+                attendant une nouvelle famille, grâce à une équipe passionnée et au soutien des adoptants et donateurs.
+            </p>
+        </div>
+        <img src="" alt="">
+    </section>
+
+    <section>
+        <h2>
+            Adoptez-nous
+        </h2>
+        <div class="card">
+            <img src="" alt="">
+            <div>
+                <div>
+                    <p>
+                        Balou
+                    </p>
+                    <p>
+                        A adopter
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        6 ans
+                    </p>
+                    <p>
+                        Frenchie
+                    </p>
+                    <p>
+                        Masculin
+                    </p>
+                </div>
+                <a href="">
+                    Voir plus d'infos
+                    <svg width="35" height="26" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 35 26"
+                         class="arrow_discover">
+                        <path class="st0" d="M2.2,13.3h28M30.2,13.3L16.2,2.3M30.2,13.3l-14,11.1"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </section>
+@endcomponent
