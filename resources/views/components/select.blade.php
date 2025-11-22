e50ba49c254e607cee802f302f8f7627632ec8ff
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-11-22 10:11:10 +0100

    add css to select

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -4,14 +4,16 @@
     'options' => [],
 ])
 
-<label for="{{$select_name}}" class="">{{$label}}</label>
+<label for="{{$select_name}}" class="fw-700 d-block p-b-16">{{$label}}</label>
 
-<select name="{{$select_name}}" id="{{$select_name}}" class="">
-    <option class="" value="">--Sélectionnez une option--</option>
+<select name="{{$select_name}}" id="{{$select_name}}" class="d-block background-white border-r-big p-16-32">
+    <option class="m-b-24" value="">--Sélectionnez une option--</option>
 
     @foreach($options as $option)
         <x-select-option :option_value="$option['value']" :option_name="$option['name']"  />
 
     @endforeach
+
+   {{-- <option value="{{$value}}">{{$option_name}}</option>--}}
 
 </select>
