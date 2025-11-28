c9f9b852e4eca81da2f0de28f8ff8cad7703d1ed
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-11-28 11:45:14 +0100

    fixing link to navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,20 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content="Les pattes heureuses, refuge, animal shelter, animals, cat, dog, chien, chat, animaux, bénévolat">
+    <meta name="keywords"
+          content="Les pattes heureuses, refuge, animal shelter, animals, cat, dog, chien, chat, animaux, bénévolat">
     <meta name="author" content="Lisa-Marie Kreins">
     <meta name="description" content="La page du réfuge 'Les pattes heureuses'">
     <title>{{ $title ?? 'Page Title' }}</title>
     @vite(['resources/css/app.css'])
 </head>
 <body class="body-style">
-@component('layouts.navigation')
-@endcomponent
+<x-navigation></x-navigation>
 
-<main  class="body-content" id="content">
-{{ $slot }}
-    </main>
-@component('layouts.footer')
-@endcomponent
+<main class="body-content" id="content">
+    {{ $slot }}
+</main>
+
 </body>
 </html>
