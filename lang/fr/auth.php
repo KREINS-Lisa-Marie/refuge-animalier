2e29d7ff8c07111c2b0a6f47b6a0da1fa10e95c4
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-05 09:00:35 +0100

    adding basic translation files

---
+++ lang/fr/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'L’email et le mot de passe ne correspondent pas.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'forgotten_password' => 'Mot de passe oublié',
+    'reset_password'=>'Reset mot de passe',
+
+];
