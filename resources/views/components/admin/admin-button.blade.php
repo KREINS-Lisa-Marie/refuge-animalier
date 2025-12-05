cf80dce510625009f7c38d41e28ca8b34f567f38
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-05 15:13:42 +0100

    adding admin link button component

---
+++ resources/views/components/admin/admin-button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{$href}}" title="{{$title}}" hreflang="{{ app()->getLocale()  }}" class="dark-button-background color-white border-r-big admin-button fw-medium t-a-center">
+    {!! $slot !!}
+</a>
