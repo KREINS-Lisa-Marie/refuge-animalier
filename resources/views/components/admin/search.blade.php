687ec8f7f3016d8468332faf5de20241548fabb3
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-05 15:12:55 +0100

    creating search component

---
+++ resources/views/components/admin/search.blade.php
@@ -0,0 +1,4 @@
+<label for="search" class="sro">
+    Recherche
+</label>
+<input type="text" name="search" id="search" class="background-white border-r-big admin-search t-a-center"{{-- value="{{/*$old || */}}"--}} placeholder="Rechercher">
