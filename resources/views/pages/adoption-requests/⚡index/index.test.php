249e1a6103df9d2ed57d1d1a6f2043dc1434413a
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-09 20:53:12 +0100

    creating test for adoption request page

---
+++ resources/views/pages/adoption-requests/⚡index/index.test.php
@@ -0,0 +1,15 @@
+<?php
+
+use Livewire\Livewire;
+
+it('renders successfully', function () {
+    Livewire::test('pages::adoption-requests.index')
+        ->assertStatus(200);
+});
+
+it('verifies that the volunteers page is showing content elements in the right order', function () {
+    Livewire::test('pages::adoption-requests.index')
+        ->assertStatus(200)
+        ->assertSee(['Demandes d’adoption', 'Créer', 'Rechercher', 'Nom', 'Date', 'Statut' ]);
+});
+
