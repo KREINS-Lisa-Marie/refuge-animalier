0a537f5ec27c8c354fa49418604a00fa2312a8d7
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-12 09:08:00 +0100

    adding translations for component select

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -7,7 +7,7 @@
 <label for="{{$select_name}}" class="fw-700 d-block p-b-16">{{$label}}</label>
 
 <select name="{{$select_name}}" id="{{$select_name}}" class="d-block background-white border-r-big p-16-32">
-    <option class="m-b-24" value="">--Sélectionnez une option--</option>
+    <option class="m-b-24" value="">{{__('components/select.select_an_option')}}</option>
 
     @foreach($options as $option)
         <x-select-option :option_value="$option['value']" :option_name="$option['name']"  />
