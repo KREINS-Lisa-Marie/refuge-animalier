8fbc36fb8318d78e7bdc87e1f56cc8584b9d2364
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-17 16:15:18 +0100

    create route for animals show page

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,8 @@ Route::livewire('/{locale}/dashboard', 'pages::dashboard.index')->name('pages::d
 
 Route::livewire('/{locale}/admin/animals', 'pages::animals.index')->name('pages::animals.index');
 
+Route::livewire('/{locale}/admin/animals/{animal}/show', 'pages::animals.show')->name('pages::animals.show');
+
 Route::livewire('/{locale}/admin/volunteers', 'pages::volunteers.index')->name('pages::volunteers.index');
 
 Route::livewire('/{locale}/admin/volunteers/{volunteer}/show', 'pages::volunteers.show')->name('pages::volunteers.show');
