829cd7e433b91a61e31ee0102b471238ac6956b8
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-17 11:28:37 +0100

    adding translation files

---
+++ lang/en/admin/sidebar.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'les_pattes_heureuses' => 'Les pattes heureuses',
+    'main_navigation' => 'Main navigation',
+    'go_to_main_content' => 'Go to main content',
+    'dashboard' => 'Dashboard',
+    'the_animals' => 'Animals',
+    'adoption_requests' => 'Adoption requests',
+    'messages' => 'Messages',
+    'volunteers' => 'Volunteers',
+    'profile' => 'Profile',
+    'logout' => 'Log out',
+
+    'go_to_the_page_profile' => 'Go to the Profile page',
+    'go_to_the_page_messages' => 'Go to the Messages page',
+    'go_to_the_page_volunteers' => 'Go to the Volunteers page',
+    'go_to_the_page_adoption_requests' => 'Go to the Adoption Requests page',
+    'go_to_the_page_animals' => 'Go to the Animals page',
+    'go_to_the_page_dashboard' => 'Go to the Dashboard page',
+
+    'logo' => 'Les pattes heureuses Logo — Go to the Dashboard',
+    'go_to_homepage' => 'Go to the Dashboard page',
+];
---
+++ lang/fr/admin/sidebar.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'les_pattes_heureuses' => 'Les pattes heureuses',
+    'main_navigation' => 'Navigation principale',
+    'go_to_main_content' => 'Aller au contenu principal',
+    'dashboard' => 'Dashboard',
+    'the_animals' => 'Les animaux',
+    'adoption_requests' => 'Demandes d’adoption',
+    'messages' => 'Messages',
+    'volunteers' => 'Bénévoles',
+    'profile' => 'Profil',
+    'logout' => 'Déconnexion',
+
+    'go_to_the_page_profile' => 'Aller vers la page Profil',
+    'go_to_the_page_messages' => 'Aller vers la page Messages',
+    'go_to_the_page_volunteers' => 'Aller vers la page Bénévoles',
+    'go_to_the_page_adoption_requests' => 'Aller vers la page Demandes d’adoption',
+    'go_to_the_page_animals' => 'Aller vers la page Les animaux',
+    'go_to_the_page_dashboard' => 'Aller vers la page Dashboard',
+
+    'logo' => 'Logo des PATTES HEUREUSES — Aller vers le Dashboard',
+    'go_to_homepage' => 'Aller vers la page Dashboard',
+];
