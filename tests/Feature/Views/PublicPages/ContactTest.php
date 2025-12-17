bef978f82b419d2307558edeae9b45366f6ed66c
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-17 14:50:12 +0100

    fix contact page view test

--- tests/Feature/Views/PublicPages/ContactTest.php
+++ tests/Feature/Views/PublicPages/ContactTest.php
@@ -6,6 +6,6 @@ it('verifies that the contact page is showing it’s main title', function () {
     $response = $this->get(route('public.contact', ['locale' => __('general.currentLocale')]));
 
     $response->assertStatus(200)
-        ->assertSeeInOrder(["form", "Contactez-nous", "Envoyer"]);
+        ->assertSeeInOrder([ "Contactez-nous", "form", "Envoyer"]);
 
 });
