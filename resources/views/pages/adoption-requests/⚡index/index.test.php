b3757ce2f4a14058e708d76382ee85689c25c826
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-23 16:59:36 +0100

    fixing typo

--- resources/views/pages/adoption-requests/⚡index/index.test.php
+++ resources/views/pages/adoption-requests/⚡index/index.test.php
@@ -7,7 +7,7 @@ it('renders successfully', function () {
         ->assertStatus(200);
 });
 
-it('verifies that the volunteers page is showing content elements in the right order', function () {
+it('verifies that the adoption-requests page is showing content elements in the right order', function () {
     Livewire::test('pages::adoption-requests.index')
         ->assertStatus(200)
         ->assertSee(['Demandes d’adoption', 'Créer', 'Rechercher', 'Nom', 'Date', 'Statut' ]);
