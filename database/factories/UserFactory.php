3d77575b665c3b1bc8c14a8fc644a9a83a1a0f8a
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-23 20:50:19 +0100

    fixed factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,14 +26,14 @@ class UserFactory extends Factory
         return [
             'first_name' => fake()->firstName(),
             'last_name' => fake()->lastName(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => fake()->unique()->email(),
             'email_verified_at' => now(),
             'phone'=> fake()->phoneNumber(),
             'role'=> fake()->jobTitle(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'profile_image' => fake()->image(),
-            'is_admin' => fake()->boolean()
+            'profile_image' => fake()->imageUrl(),
+            'is_admin' => fake()->boolean(),
         ];
     }
 
