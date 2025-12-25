ff967cbaccd76f666cb417a0aeb2381a43866eec
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 16:53:20 +0100

    get messages data

--- resources/views/pages/messages/⚡index/index.php
+++ resources/views/pages/messages/⚡index/index.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Models\Message;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 new #[Layout('layouts.dashboard')] class extends Component
 {
-    //
+    public $messages;
+
+    public function mount(): void
+    {
+        $this->messages = Message::get();
+    }
 };
