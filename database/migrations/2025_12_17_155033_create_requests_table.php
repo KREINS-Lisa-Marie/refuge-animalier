49049cd91a83b270854e2549b0db3218d79a4a25
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 15:43:28 +0100

    fixing migration for requests

--- database/migrations/2025_12_17_155033_create_requests_table.php
+++ database/migrations/2025_12_17_155033_create_requests_table.php
@@ -9,6 +9,7 @@ return new class extends Migration {
     {
         Schema::create('requests', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('animal_id')->constrained()->cascadeOnDelete();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('state');
