409901e457d942ab66ce50761216f9535a787c66
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 15:35:52 +0100

    adding enum values into factory

--- database/factories/AnimalFactory.php
+++ database/factories/AnimalFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\AnimalState;
+use App\Enums\Sex;
 use App\Models\Animal;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
@@ -12,16 +14,21 @@ class AnimalFactory extends Factory
 
     public function definition(): array
     {
+
+        $sex = [Sex::Male->value, Sex::Female->value];
+        $state = [AnimalState::Adoptable->value, AnimalState::Adopted->value, AnimalState::ProcessingAdoption->value, AnimalState::InTreatment->value];
+
+
         return [
             'animal_name' => fake()->firstName(),
             'species' => fake()->word(),
             'race' => fake()->word(),
-            'sex' => fake()->word(),
+            'sex' => fake()->randomElement($sex),
             'fur' => fake()->word(),
-            'age' => fake()->randomNumber(),
+            'age' => fake()->numberBetween(1, 18),
             'vaccinations' => fake()->word(),
             'character' => fake()->word(),
-            'state' => fake()->word(),
+            'state' => fake()->randomElement($state),
             'description' => fake()->text(),
             'show_image' => fake()->image(),
             'gallery_images' => fake()->image(),
