62b288cb6dcbab5dca8c77a1c301a489ee1aef55
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 18:28:39 +0100

    add auth middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -34,30 +34,58 @@ Route::livewire('/{locale}/dashboard', 'pages::dashboard.index')->name('pages::d
     'auth',
 ]);
 
-Route::livewire('/{locale}/admin/animals', 'pages::animals.index')->name('pages::animals.index');
+Route::livewire('/{locale}/admin/animals', 'pages::animals.index')->name('pages::animals.index')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/animals/create', 'pages::animals.create')->name('pages::animals.create');
+Route::livewire('/{locale}/admin/animals/create', 'pages::animals.create')->name('pages::animals.create')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/animals/{animal}/show', 'pages::animals.show')->name('pages::animals.show');
+Route::livewire('/{locale}/admin/animals/{animal}/show', 'pages::animals.show')->name('pages::animals.show')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/animals/{animal}/edit', 'pages::animals.edit')->name('pages::animals.edit');
+Route::livewire('/{locale}/admin/animals/{animal}/edit', 'pages::animals.edit')->name('pages::animals.edit')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/volunteers', 'pages::volunteers.index')->name('pages::volunteers.index');
+Route::livewire('/{locale}/admin/volunteers', 'pages::volunteers.index')->name('pages::volunteers.index')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/volunteers/{volunteer}/show', 'pages::volunteers.show')->name('pages::volunteers.show');
+Route::livewire('/{locale}/admin/volunteers/{volunteer}/show', 'pages::volunteers.show')->name('pages::volunteers.show')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/volunteers/create', 'pages::volunteers.create')->name('pages::volunteers.create');
+Route::livewire('/{locale}/admin/volunteers/create', 'pages::volunteers.create')->name('pages::volunteers.create')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/volunteers/{volunteer}/edit', 'pages::volunteers.edit')->name('pages::volunteers.edit');
+Route::livewire('/{locale}/admin/volunteers/{volunteer}/edit', 'pages::volunteers.edit')->name('pages::volunteers.edit')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/adoption-requests', 'pages::adoption-requests.index')->name('pages::adoption-requests.index');
+Route::livewire('/{locale}/admin/adoption-requests', 'pages::adoption-requests.index')->name('pages::adoption-requests.index')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/adoption-requests/create', 'pages::adoption-requests.create')->name('pages::adoption-requests.create');
+Route::livewire('/{locale}/admin/adoption-requests/create', 'pages::adoption-requests.create')->name('pages::adoption-requests.create')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/adoption-requests/{adoption_request}/edit', 'pages::adoption-requests.edit')->name('pages::adoption-requests.edit');
+Route::livewire('/{locale}/admin/adoption-requests/{adoption_request}/edit', 'pages::adoption-requests.edit')->name('pages::adoption-requests.edit')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/messages', 'pages::messages.index')->name('pages::messages.index');
+Route::livewire('/{locale}/admin/messages', 'pages::messages.index')->name('pages::messages.index')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/profile', 'pages::profile.index')->name('pages::profile.index');
+Route::livewire('/{locale}/admin/profile', 'pages::profile.index')->name('pages::profile.index')->middleware([
+    'auth',
+]);
 
-Route::livewire('/{locale}/admin/profile/{profile}/edit', 'pages::profile.edit')->name('pages::profile.edit');
+Route::livewire('/{locale}/admin/profile/{profile}/edit', 'pages::profile.edit')->name('pages::profile.edit')->middleware([
+    'auth',
+]);
