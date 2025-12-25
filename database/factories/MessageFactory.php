8b1b918e8ef89acb87ce85bc8a4462fb8863a9ff
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 15:36:24 +0100

    adding enum values into message factory

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\MessageState;
 use App\Models\Message;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
@@ -12,12 +13,16 @@ class MessageFactory extends Factory
 
     public function definition(): array
     {
+
+        $state = [MessageState::Read->value, MessageState::NotReadYet->value];
+
         return [
             'first_name' => fake()->firstName(),
             'last_name' => fake()->word(),
             'subject' => fake()->word(),
             'message' => fake()->word(),
             'email' => fake()->unique()->safeEmail(),
+            'state' => fake()->randomElement($state),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
