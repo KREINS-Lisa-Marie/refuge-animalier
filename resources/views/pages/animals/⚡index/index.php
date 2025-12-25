a2adedcfa2a7808c6adeab13b3948c323de6afdb
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 16:40:12 +0100

    get animals data for index page

--- resources/views/pages/animals/⚡index/index.php
+++ resources/views/pages/animals/⚡index/index.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Models\Animal;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 new #[Layout('layouts.dashboard')] class extends Component
 {
-    //
+    public $animals;
+
+    public function mount(): void
+    {
+        $this->animals = Animal::get();
+    }
 };
