007112883a12fd550d884a03e22c1e15ff351e08
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 16:35:50 +0100

    get volunteers data for edit

--- resources/views/pages/volunteers/⚡edit/edit.php
+++ resources/views/pages/volunteers/⚡edit/edit.php
@@ -1,9 +1,19 @@
 <?php
 
+use App\Models\Availability;
+use App\Models\User;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 new #[Layout('layouts.dashboard')] class extends Component
 {
-    //
+
+    public User $volunteer;
+    public Availability $availabilities;
+
+    public function mount(User $volunteer): void
+    {
+        $this->volunteer = $volunteer;
+        $this->availabilities = Availability::where('user_id', $volunteer->id)->first();
+    }
 };
