c007679e2a206805a3f433ad752a19a4ccf3ccc3
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-25 15:37:09 +0100

    adding enum values into request factory

--- database/factories/RequestFactory.php
+++ database/factories/RequestFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\RequestState;
 use App\Models\Animal;
 use App\Models\Request;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -13,10 +14,13 @@ class RequestFactory extends Factory
 
     public function definition(): array
     {
+
+        $state = [RequestState::Refused->value, RequestState::Adopted->value, RequestState::InTreatment->value, RequestState::NotTreatedYet->value];
+
         return [
             'first_name' => fake()->firstName(),
             'last_name' => fake()->lastName(),
-            'state' => fake()->word(),
+            'state' => fake()->randomElement($state),
             'email' => fake()->unique()->safeEmail(),
             'phone' => fake()->phoneNumber(),
             'date' => Carbon::now(),
