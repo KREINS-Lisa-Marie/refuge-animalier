feb237a2bc28c3ce7a61ada6d91eb9fe3816b13d
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-27 20:40:38 +0100

    create search logic for index page

--- resources/views/pages/volunteers/⚡index/index.php
+++ resources/views/pages/volunteers/⚡index/index.php
@@ -1,15 +1,38 @@
 <?php
 
 use App\Models\User;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
-new  #[Layout('layouts.dashboard')] class extends Component
+new  #[Layout('layouts.app')] class extends Component
 {
     public $volunteers;
+    public string $term = '';
 
     public function mount(): void
     {
         $this->volunteers = User::get();
     }
+
+
+
+
+
+    #[Computed]
+    public function searchedUsers()
+    {
+        return
+            User::query()
+                ->where('first_name', 'like', '%' . $this->term . '%')
+                ->orWhere('last_name', 'like', '%' . $this->term . '%')
+                ->orWhere('phone', 'like', '%' . $this->term . '%')
+                ->orWhere('is_admin', 'like', '%' . $this->term . '%')
+                ->orderBy('created_at', 'asc')
+                ->get();
+    }
+
+
+
+
 };
