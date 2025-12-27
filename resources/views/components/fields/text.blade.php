bfd29ca43a4aa1ed5aa0fa2646ef51cabe4a323c
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-27 20:48:17 +0100

    added error message

--- resources/views/components/fields/text.blade.php
+++ resources/views/components/fields/text.blade.php
@@ -2,9 +2,11 @@
     <label for="{!! $name !!}" class="field__label">
         {!! $slot!!}
     </label>
-    <input wire:model.blur="{{$wire}}" type="text" name="{{$name}}" id="{{$id}}" value="{{$value}}" class="field__input" placeholder="{{$placeholder}}"
+    <input wire:model.blur="{{$wire}}" type="text" name="{{$name}}" id="{{$id}}" value="{{$value ?? ''}}" class="field__input" placeholder="{{$placeholder}}"
            aria-required="true">
-
+    @error($name)
+    {{$message}}
+    @enderror
 
     {{--
     wire blurr heisst dass das das formulaire schon an server schickt wenn aus dem feld raus geht also kann das theoretisch button ersetzen aber besser muss auf button whire submit machen!!!
