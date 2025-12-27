2e9c8d7578c1c943e0033c1c9f46434242112c52
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-27 20:47:06 +0100

    add cascade

--- database/migrations/0001_01_01_000001_create_availabilities_table.php
+++ database/migrations/0001_01_01_000001_create_availabilities_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('availabilities', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('monday')->nullable();
             $table->string('tuesday')->nullable();
             $table->string('wednesday')->nullable();
