d5e3171dac93518d5006a1b15ff9160f2a0137f1
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-28 11:40:11 +0100

    create animals index logic

--- resources/views/pages/animals/⚡index/index.php
+++ resources/views/pages/animals/⚡index/index.php
@@ -1,15 +1,33 @@
 <?php
 
 use App\Models\Animal;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
-new #[Layout('layouts.dashboard')] class extends Component
+new #[Layout('layouts.app')] class extends Component
 {
     public $animals;
+    public string $term = '';
+
 
     public function mount(): void
     {
         $this->animals = Animal::get();
     }
+
+
+    #[Computed]
+    public function searchedAnimals()
+    {
+        return
+            Animal::query()
+                ->where('animal_name', 'like', '%' . $this->term . '%')
+                ->orWhere('species', 'like', '%' . $this->term . '%')
+                ->orWhere('state', 'like', '%' . $this->term . '%')
+                ->orderBy('created_at', 'asc')
+                ->get();
+    }
+
+
 };
