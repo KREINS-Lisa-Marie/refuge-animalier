943d39716310ccfbe68aeadaca431ad520958c78
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-12-28 11:40:54 +0100

    create animals create logic

--- resources/views/pages/animals/⚡create/create.php
+++ resources/views/pages/animals/⚡create/create.php
@@ -1,9 +1,59 @@
 <?php
 
+use App\Http\Requests\StoreAnimalRequest;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
-new #[Layout('layouts.dashboard')] class extends Component
+new #[Layout('layouts.app')] class extends Component
 {
-    //
+
+    public string $animal_name;
+    public string $species;
+    public string $race = '';
+    public string $sex;
+    public string $fur = '';
+    public string $age;
+    public string $vaccinations = '';
+    public string $character = '';
+    public string $state;
+    public string $description = '';
+    public string $show_image = '';
+    public string $gallery_images = '';
+
+
+
+    public function store(): void
+    {
+        $this->validate([
+            'animal_name'=>'required|string|max:255',
+            'species'=>'string|required',
+            'race'=>'string|nullable',
+            'sex'=>'string|required',
+            'fur'=>'string|nullable',
+            'age'=>'string|required',
+            'vaccinations'=>'string|nullable',
+            'character'=>'string|nullable',
+            'state'=>'string|required',
+            'description'=>'string|nullable',
+            'show_image'=>'nullable|image',
+            'gallery_images'=>'nullable|image',
+        ]);
+
+        \App\Models\Animal::create([
+            'animal_name' => $this->animal_name,
+            'species' => $this->species,
+            'race' => $this->race,
+            'sex' => $this->sex,
+            'fur' => $this->fur,
+            'age' => $this->age,
+            'vaccinations' => $this->vaccinations,
+            'character' => $this->character,
+            'state' => $this->state,
+            'description' => $this->description,
+            'show_image' => $this->show_image,
+            'gallery_images' => $this->gallery_images,
+        ]);
+
+        $this->redirect(route('pages::animals.index', ['locale' => __('general.currentLocale')]));
+    }
 };
