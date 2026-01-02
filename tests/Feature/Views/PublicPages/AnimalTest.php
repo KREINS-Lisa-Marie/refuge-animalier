844ad9a170fd7c0f289424a7b61ef0daf8031b5d
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-01-02 09:37:02 +0100

    add comment

--- tests/Feature/Views/PublicPages/AnimalTest.php
+++ tests/Feature/Views/PublicPages/AnimalTest.php
@@ -2,9 +2,10 @@
 
 it('verifies that the animal page is showing content elements in the right order', function () {
 
-
+    //act
     $response = $this->get(route('public.animal', ['locale' => __('general.currentLocale')]));
 
+    //assert
     $response->assertStatus(200)
         ->assertSeeInOrder(["Race", "Caractère", "Galerie"]);
 
