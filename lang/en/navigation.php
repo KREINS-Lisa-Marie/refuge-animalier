ea457efb82a48ac715de13820064f3d43827c380
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-07-08 11:30:44 +0200

    translate navigation

---
+++ lang/en/navigation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'main_navigation' => 'Main navigation',
+    'logo_back_to_home' => 'PATTES HEUREUSES logo — Go to the homepage',
+    'go_to_home' => 'Go to the homepage',
+    'go_to_main_content' => 'Go to main content',
+    'home' => 'HOME',
+    'our_animals' => 'OUR ANIMALS',
+    'contact' => 'CONTACT',
+    'go_to_contact' => 'Go to the Contact page',
+    'go_to_animals' => 'Go to the Our Animals page',
+];
---
+++ lang/fr/navigation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'main_navigation' => 'Navigation principale',
+    'logo_back_to_home' => 'Logo des PATTES HEUREUSES — Aller vers la page d’accueil',
+    'go_to_home' => 'Aller vers la page d’accueil',
+    'go_to_main_content' => 'Aller au contenu principal',
+    'home' => 'ACCUEIL',
+    'our_animals' => 'NOS ANIMAUX',
+    'contact' => 'CONTACT',
+    'go_to_contact' => 'Aller vers la page Contact',
+    'go_to_animals' => 'Aller vers la page Nos animaux',
+];
