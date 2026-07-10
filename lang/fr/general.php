7aa20711a8b587f86090177c7fcee6a659f53105
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-07-10 23:01:08 +0200

    adding page titles

--- lang/fr/general.php
+++ lang/fr/general.php
@@ -2,4 +2,29 @@
 
 return [
     'currentLocale' => 'fr',
+
+    /*Page titles*/
+
+    'dashboard'=>'Dashboard — Les pattes heureuses',
+    'animals'=>'Animaux — Les pattes heureuses',
+    'animals_create'=>'Créer un animal — Les pattes heureuses',
+    'animals_edit'=>'Modifier un animal — Les pattes heureuses',
+    'animals_show'=>'Détail de l’animal — Les pattes heureuses',
+    'adoption-requests'=>'Demandes d’adoption — Les pattes heureuses',
+    'adoption-requests_create'=>'Créer une demande d’adoption — Les pattes heureuses',
+    'adoption-requests_edit'=>'Modifier une demande d’adoption — Les pattes heureuses',
+    'volunteers'=>'Bénévoles — Les pattes heureuses',
+    'volunteers_detail'=>'Détail du Bénévole — Les pattes heureuses',
+    'volunteers_edit'=>'Modifier un bénévole — Les pattes heureuses',
+    'volunteers_create'=>'Créer un bénévole — Les pattes heureuses',
+    'messages'=>'Messages — Les pattes heureuses',
+    'profile'=>'Profil — Les pattes heureuses',
+    'profile-edit'=>'Modifier mon profil — Les pattes heureuses',
+    'homepage'=>'Les pattes heureuses',
+    'our_animals'=>'Nos animaux',
+    'animal_details'=>'Details de l’animal',
+
+    'legals'=>'Mentions légales',
+    'contact'=>'Contactez-nous',
+
 ];
