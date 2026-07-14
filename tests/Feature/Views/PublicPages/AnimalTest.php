6b935728a721b5340641b66d2550c000e616609b
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-07-14 15:47:35 +0200

    fixing public view tests

--- tests/Feature/Views/PublicPages/AnimalTest.php
+++ tests/Feature/Views/PublicPages/AnimalTest.php
@@ -2,8 +2,10 @@
 
 it('verifies that the animal page is showing content elements in the right order', function () {
 
+    $animal = \App\Models\Animal::factory()->create();
+
     //act
-    $response = $this->get(route('public.animal', ['locale' => __('general.currentLocale')]));
+    $response = $this->get(route('public.animal', ['locale' => __('general.currentLocale'), 'animal' => $animal]));
 
     //assert
     $response->assertStatus(200)
--- tests/Feature/Views/PublicPages/AnimalsTest.php
+++ tests/Feature/Views/PublicPages/AnimalsTest.php
@@ -2,8 +2,9 @@
 
 it('verifies that the animals page is showing content elements in the right order', function () {
 
+$animals = \App\Models\Animal::factory(10)->create();
 
-    $response = $this->get(route('public.animals', ['locale' => __('general.currentLocale')]));
+    $response = $this->get(route('public.animals', ['locale' => __('general.currentLocale')], compact('animals')));
 
     $response->assertStatus(200)
         ->assertSeeInOrder(["Nos animaux", "Age", "Rechercher"]);
