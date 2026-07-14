d6b7c2b84037e18983b2449f3583fa7c1f061292
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-07-14 16:23:17 +0200

    config image details

---
+++ config/animalimage.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'jpg_image_type' => 'jpg',
+    'jpg_compression' => 80,
+    'originals_path' => 'images/animals/originals',
+    'variants_path_pattern' => 'images/animals/variants/%sx%s',
+    'sizes' => [
+        'small' => [
+            'height' => 100,
+            'width' => 100,
+        ],
+        'medium' => [
+            'height' => 200,
+            'width' => 200,
+        ],
+        'large' => [
+            'height' => 334,
+            'width' => 334,
+        ],
+        'xl' => [
+            'height' => 400,
+            'width' => 400,
+        ],
+    ],
+];
