929aa96c342f464344c3ff6eeb96e362595cd367
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-08-02 22:18:29 +0200

    improve edit and add images

--- resources/views/pages/animals/⚡edit/edit.blade.php
+++ resources/views/pages/animals/⚡edit/edit.blade.php
@@ -3,7 +3,7 @@
         Modifier {{ $animal_name }}
     </x-page-bar>
 
-    <form wire:submit.prevent="save" class="profile-form volunteers-edit" enctype="multipart/form-data">
+    <form wire:submit.prevent="save" class="admin-form volunteers-edit" enctype="multipart/form-data">
         @csrf
     <fieldset class="profile-information max-w-admin-web  edit-inputs ">
         <legend class="fw-700 admin-dashboard-title">
@@ -61,24 +61,69 @@
                 <x-select select_name="state" label="{{__('admin/animals.state')}}" wire="state"
                           :options="$animal_state_options">
                 </x-select>
+                <x-fields.file name_id="show_image" wire="show_image" name="show_image">
+                    {{__('admin/animals.animal_image')}}
+                </x-fields.file>
             </div>
         </div>
-{{--        <x-fields.file name_id="show_image" wire="show_image" name="volunteer_img">
-            {{__('admin/animals.animal_image')}}
-        </x-fields.file>--}}
     </fieldset>
 
     <fieldset class="profile-information max-w-admin-web  edit-inputs ">
         <legend class="fw-700 admin-dashboard-title">
             {{__('admin/animals.gallery')}}
         </legend>
-       {{-- <x-fields.file name_id="volunteer_img" wire="volunteer_img" name="volunteer_img">
+
+        <x-fields.multifile name_id="new_gallery_images" wire="new_gallery_images" name="new_gallery_images">
             {{__('admin/animals.animal_image')}}
-        </x-fields.file>--}}
+        </x-fields.multifile>
+        @error('gallery_images')
+        <p class="error">
+            {{$message}}
+        </p>
+        @enderror
+        @error('gallery_images'.'.*')
+        <p class="error">
+            {{$message}}
+        </p>
+        @enderror
+        @if(!empty($gallery_images))
+            <div class="gallery-preview">
+                @foreach($gallery_images as $index=>$image)
+                    <div>
+                        @if(is_string($image))
+                            <img src="{{ asset('storage/' . $image) }}" alt="Gallery image" width="100" height="100">
+                        @else
+                            @if($image->isPreviewable())
+                                <img src="{{ $image->temporaryUrl() }}" alt="Gallery image" width="100" height="100">
+                            @else
+                                <p>
+                                    {{__('admin/animals.image_cant_be_previewed')}}
+                                </p>
+                            @endif
+                        @endif
+
+                           {{--temporaryUrl parce que sinon ça ne marche pas parce que l'image n'est pas encore enregistré --}}
+                        <button type="button" wire:click="removeFromGallery({{$index}})" class="remove-img-btn" title="Delete image">
+                            X
+                        </button>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <p>{{__('admin/animals.no_images_chosen')}}</p>
+        @endif
     </fieldset>
-
-
+        <fieldset class="profile-information max-w-admin-web edit-inputs edit-textarea-big request-edit-comment">
+            <x-fields.textarea wire="internal_notes" id="internal_notes" name="internal_notes" placeholder="{{__('admin/animals.internal_notes')}}" >
+                {{__('admin/animals.internal_notes')}}
+            </x-fields.textarea>
+            <x-fields.textarea wire="modification_request" id="modification_request" name="modification_request" placeholder="{{__('admin/animals.modification_request')}}" >
+                {{__('admin/animals.modification_request')}}
+            </x-fields.textarea>
+        </fieldset>
         <div class=" max-w-admin-web volunteer-buttons top-row">
+            <x-select select_name="published_animal" :options="$published_animal_options" wire="published_animal" label="{{__('admin/animals.published_animal')}}*">
+            </x-select>
             <x-admin.form-button>
                 {{__('admin/animals.save')}}
             </x-admin.form-button>
@@ -86,3 +131,11 @@
     </form>
 
 </main>
+{{--
+
+isPreviewable()  ->  TemporaryUploadedFile (namespace Livewire\Features\SupportFileUploads)
+https://livewire.laravel.com/docs/4.x/uploads#handling-multiple-files
+https://livewire.laravel.com/docs/4.x/uploads#temporary-preview-urls
+
+
+--}}
