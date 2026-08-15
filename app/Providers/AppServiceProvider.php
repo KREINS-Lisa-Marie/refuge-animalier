74f8e818205ee9d030722045ff3b4263b7fcaefc
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-08-15 12:07:24 +0200

    finish dashboard policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Policies\DashboardPolicy;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Gate::define('view-dashboard', [DashboardPolicy::class, 'viewAny']);
+        \Gate::define('view-dashboard-limited', [DashboardPolicy::class, 'viewAnyLimited']);
     }
 }
