b6ea51576e43c0606f8b7ad6a744cc9b7cc1c3c4
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-08-20 23:30:11 +0200

    fix

--- resources/views/components/fields/multifile.blade.php
+++ resources/views/components/fields/multifile.blade.php
@@ -3,7 +3,7 @@
         {!! $slot !!}
     </label>
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000000"/>
-    <input type="file" wire:model.live="{{$wire}}" id="{{$name}}" name="{{$name}}"
+    <input type="file" wire:model="{{$wire}}" id="{{$name}}" name="{{$name}}"
            class="file-input background-white border-r-big">
     @error($name)
     <p class="error">
