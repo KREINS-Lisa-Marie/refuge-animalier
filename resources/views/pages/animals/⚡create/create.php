8a94e604349d267c9da763296820b5374ebbdeb9
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2026-08-23 15:32:32 +0200

    try fix

--- resources/views/pages/animals/⚡create/create.php
+++ resources/views/pages/animals/⚡create/create.php
@@ -102,17 +102,15 @@ new #[Layout('layouts.app')] class extends Component
         $this->gallery_images = array_values($this->gallery_images);        //faut renumérer par ce que sinon ça bug
     }
 
-    public $new_gallery_images =[];
+    public $new_gallery_image = null;
 
-    public function updatedNewGalleryImages()
+    public function updatedNewGalleryImage()
     {
-        if (is_array($this->new_gallery_images)){
-        foreach ($this->new_gallery_images as $image) {
-            $this->gallery_images[] = $image;
-        }   // ajouter les nouvelles images au array
-        }
-        $this->new_gallery_images = [];
+        if ($this->new_gallery_image) {
+            $this->gallery_images[] = $this->new_gallery_image;
+            $this->new_gallery_image = null;
         //vider array après
+        }
     }
 
 
